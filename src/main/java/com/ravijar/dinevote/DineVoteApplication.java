fbb60de785df0eeb6d95d3e6f0d13caa110ab254
ravijar <ravijarandinu@gmail.com>
2024-06-06 15:19:28 +0530

    initial commit

---
+++ src/main/java/com/ravijar/dinevote/DineVoteApplication.java
@@ -0,0 +1,13 @@
+package com.ravijar.dinevote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DineVoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DineVoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ravijar/dinevote/DineVoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravijar.dinevote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DineVoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
